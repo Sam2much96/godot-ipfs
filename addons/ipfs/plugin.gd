f857f20494314908c3dc9500313fa62fb1822b9f
sam2much96 <sam2much96@gmail.com>
2023-03-19 13:53:07 +0100

    new file:   addons/ipfs/async_executor.gdns 	new file:   addons/ipfs/delete-ipfs.gdnlib 	new file:   addons/ipfs/gdnative_lib.tres 	new file:   addons/ipfs/ipfs.gdns 	new file:   addons/ipfs/plugin.cfg 	new file:   addons/ipfs/plugin.gd 	deleted:    addons/my_custom_node/custom_node.gdns 	modified:   Cargo.lock 	modified:   Main.tscn 	deleted:    addons/my_custom_node/plugin.cfg 	modified:   icon.png.import 	deleted:    making_plugins-custom_node_icon.png 	deleted:    making_plugins-custom_node_icon.png.import 	deleted:    my_button.gdns 	deleted:    native_plugin.gdnlib 	modified:   project.godot

---
+++ addons/ipfs/plugin.gd
@@ -0,0 +1,70 @@
+tool
+extends EditorPlugin
+
+
+#var _editor_view
+
+#func _init():
+	#add_autoload_singleton('DocsHelper', "res://addons/algodot/Documentation/Scripts/DocsHelper.gd")
+	#*********For Built in Documentation**********#
+	
+	#_add_custom_editor_view()
+	
+	#"Adds a Custom Tab for Documentations"
+	#get_editor_interface().get_editor_viewport().add_child(_editor_view)
+	#make_visible(false)
+
+func _enter_tree():
+
+	var gui = get_editor_interface().get_base_control()
+	var node_icon = gui.get_icon("Node", "EditorIcons")
+
+	add_custom_type(
+		"IPFS",
+		"Node",
+		preload("res://addons/ipfs/ipfs.gdns"),
+		node_icon
+	)
+	add_autoload_singleton("AsyncExecutorDriver", "res://addons/ipfs/async_executor.gdns")
+
+
+
+
+func _exit_tree():
+	
+	remove_custom_type("IPFS")
+	#remove_autoload_singleton("AsyncExecutorDriver") #causes leaked memory bug
+
+#************For Builtin Documentation***********#
+	
+	#remove_autoload_singleton("DocsHelper")
+	#_remove_custom_editor_view()
+
+
+
+
+#***********For Builtin Documentation***********#
+func get_plugin_name()-> String:
+	return "IPFS"
+
+
+#func _add_custom_editor_view(): 
+#	_editor_view = preload("res://addons/algodot/Documentation/Scripts/DocumentationViewer.tscn").instance()
+
+	
+
+
+#func _remove_custom_editor_view():
+#	if _editor_view:
+#		_editor_view.queue_free()
+
+#func has_main_screen()-> bool:
+#	return true
+
+#func make_visible(visible: bool) -> void:
+#	if _editor_view:
+#		_editor_view.visible=visible
+
+#func get_plugin_icon()-> Texture:
+#	return get_editor_interface().get_base_control().get_icon("Spatial", "EditorIcons")
+
